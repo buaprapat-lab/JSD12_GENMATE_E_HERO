6e94ffebe4301c07ac875f20448451ce40974c47
Sahatsawat-Wattana <sahatsawat.wattanapraditchai@gmail.com>
2026-04-08 13:48:26 +0700

    Add parent class

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Hero {
-    
+    constructor(name,gender,multiverse,hp,attack) {
+        this.name = name;
+        this.gender = gender;
+        this.multiverse = multiverse;
+        this.hp = hp;
+        this.attack = attack
+    }
+    introduce() {
+        console.log(`I'm ${this.name}. I'm ${this.gender}. I'm from ${this.multiverse}.`);
+    }
+    shiftMultiverse(anothermultiverse) {
+        console.log(`I'm from ${this.multiverse}. Go to ${anothermultiverse} to save the days.`)
+    }
+    gotattacked(attack) {
+        this.hp -= attack;
+        console.log(`I got attacked ${attack} hp. I currently have ${this.hp} hp.`)
+    }
+    statusNow() {
+        if (this.hp <= 0) {
+            console.log("I'm dead");
+        } else {
+            console.log(`I'm alive.I currently have ${this.hp} hp. `);
+        } 
+    }
 }
+
